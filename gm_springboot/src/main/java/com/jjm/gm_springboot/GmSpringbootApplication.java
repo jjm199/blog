27105a398a969752adde779679f4f69f5c102f22
jjm <1544589050@qq.com>
2021-11-04 19:49:28 +0800

    创建项目

---
+++ gm_springboot/src/main/java/com/jjm/gm_springboot/GmSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jjm.gm_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmSpringbootApplication.class, args);
+    }
+
+}
